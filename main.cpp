523b6ad7d096c1f712b8cf07637fd6c674b0f153
Justin Berkshire <justinwendellberkshire@gmail.com>
2022-09-14 23:07:33 -0500

    connection written, need to handle new string messages

--- main.cpp
+++ main.cpp
@@ -10,8 +10,63 @@
 #include "main.h"
 using namespace std;
 
-int main()
-{
+class i2c_node {
+    private: int address;
+    private: int freq = 100000;
+           i2c_node(uint sda, uint scl) {
+               //init pins
+               gpio_init(sda);
+               gpio_init(scl);
+               gpio_set_function(sda, GPIO_FUNC_I2C);
+               gpio_set_function(scl, GPIO_FUNC_I2C);
+               gpio_pull_up(sda);
+               gpio_pull_up(scl);
+               //set up i2c connect
+               i2c_init(i2c1, freq);
+               address = findAvailableAddress();
+               i2c_set_slave_mode(i2c1, true, address);
+           }
+    public: int getAddress() {
+        return address;
+    };
+    // public: int readCommanderMessage() {
+        
+    //     uint messageLength = i2c_get_read_available(i2c1);
+
+    //     uint8_t fullMessage[messageLength];
+
+    //     i2c_read_raw_blocking (i2c1, fullMessage, messageLength);
+
+    //     return *fullMessage;
+
+    // }
+    private: bool reserved_addr(uint8_t addr) {
+        return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+    };
+    private: int findAvailableAddress() {
+
+        for (int addr = 0; addr < (1 << 7); ++addr) {
+            if (addr % 16 == 0) {
+                printf("%02x ", addr);
+            }
+
+            int ret;
+            uint8_t rxdata;
+            if (reserved_addr(addr))
+                ret = PICO_ERROR_GENERIC;
+            else
+                ret = i2c_read_blocking(i2c0, addr, &rxdata, 1, false);
+
+            if (ret < 0) {
+                return addr;
+            }
+        }
+
+        sleep_ms(2000);
+    }
+};
+
+int main() {
     const uint LED = PICO_DEFAULT_LED_PIN;
 
     gpio_init(LED);
@@ -24,12 +79,11 @@ int main()
 
     int b = 8;
 
-    int c = a + b; 
+    int c = a + b;
 
     cout << c << '\n';
 
-    while (true)
-    {
+    while (true) {
         cout << "Deadly Virus";
         gpio_put(LED, 1);
         sleep_ms(a * 100);
